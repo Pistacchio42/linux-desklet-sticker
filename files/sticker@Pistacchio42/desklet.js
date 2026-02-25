cc8cf8d38004ca49fd7220b4efb8611efe377f16
Stefano <stefano.lamorgia@starcode.it>
2026-02-25 17:30:25 +0100

    taking inspo from picture-frame @KopfdesDaemons

--- files/sticker@Pistacchio42/desklet.js
+++ files/sticker@Pistacchio42/desklet.js
@@ -1,29 +1,53 @@
 const Settings = imports.ui.settings;
+const Desklet = imports.ui.desklet;
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
+const Gio = imports.gi.Gio;
 
 const UUID = "sticker@Pistacchio42";
 
 let image = GLib.get_user_special_dir
 
-function main(metadata, instanceId){
-    let myDesklet = new myDesklet(metadata,instanceId);
-    return myDesklet;
+function main(metadata, instanceId) {
+  return new MyDesklet(metadata, instanceId);
 }
 
-function MyDesklet(metadata, instanceId) {
-    this._init(metadata, instanceId);
-}
+class MyDesklet extends Desklet.Desklet{
 
-MyDesklet.prototype = {
+    constructor (metadata, instanceId) {
 
-    _init: function(metadata, instanceId) {
+        super(metadata, instanceId);
+
         this.settings = new Settings.DeskletSettings(this, metadata["uuid"], instanceId);
 
         this.settings.bindProperty(Settings.BindingDirection.IN, "image", "image", this.on_settings_changed, null);
         this.settings.bindProperty(Settings.BindingDirection.IN, "angle", "angle", this.on_settings_changed, null);
         this.settings.bindProperty(Settings.BindingDirection.IN, "size", "size", this.on_settings_changed, null);
 
-        this.image = this.settings.getValue("image") || "files\\sticker@Pistacchio42\\icon.png",
+        this.image = this.settings.getValue("image") || this.metadata.path + "/icon.png";
         this.angle = this.settings.getValue("angle") || 0;
         this.size = this.settings.getValue("size") || 1;
+
+        this.setHeader(_("Sticker"));
+        this._initUI();
     }
+
+    _initUI(){
+        const mainContainer = new St.BoxLayout({ vertical: true });
+
+        const size = this.size;
+        const angle = this.angle;
+        const finalImagePath = decodeURIComponent(this.image.replace("file://", ""));
+        const imageActor = this.createImage(finalImagePath, size, angle);
+
+        mainContainer.add_child(imageActor);
+        this.setContent(mainContainer);
+    }
+
+    createImage(imagePath, size, angle){
+
+        const canvas = new Clutter.Canvas();
+
+    }
+
 }
